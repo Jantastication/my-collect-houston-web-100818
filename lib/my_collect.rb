57ffe951b2a33df71f07fa7035674b86fdbbadd0
Jantastication O <georginaoregx@gmail.com>
2018-10-04 04:03:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,9 @@
 def my_collect(collection)
-  my_collect(collection) do |language|
-    language.upcase
+  i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection <<yield(collection[i])
+    i += 1
   end
+ new_collection
+end
